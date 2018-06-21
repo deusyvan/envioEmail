31678790bdb3b1698c50f020f59fbaf1f314fb82
deusyvan <deusyvan@gmail.com>
2018-06-20 22:40:22 -0300

    Criando formulário simples

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+
+
+?>
+
+<form method="POST">
+
+	NOME:<br/>
+	<input type="text"  name="nome"/><br/><br/>
+	
+	E-MAIL:<br/>
+	<input type="email"  name="email"/><br/><br/>
+	
+	MENSAGEM:<br/>
+	<textarea name="msg"></textarea><br/><br/>
+	
+	<input type="submit"  value="Enviar"/>
+
+</form>
